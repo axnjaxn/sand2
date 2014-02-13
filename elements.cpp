c542b461145a379ff579659958b47067f252b561
Brian Jackson <axnjaxn@axnjaxn.com>
2014-02-12 21:49:58 -0500

    Converting the Sand2Spec to ElementTable is done.

--- elements.cpp
+++ elements.cpp
@@ -1,5 +1,6 @@
 #include "randomizer.h"
 #include "elements.h"
+#include <exception>
 
 ElementID ProbList::random() {
   float f = Randomizer::uniform();
@@ -17,13 +18,17 @@ Element::Element(const std::string& name) {
 }
 
 ElementTable::ElementTable(Sand2Spec* spec) {
+  std::string name;
+  int i, j, k;
+
   /*
    * Step 1: Get the element names (so that they can be bound)
    */
   elements.push_back(Element("Air"));
   elements.push_back(Element("Wall"));
   for (S2List* lst = spec->properties; lst; lst = lst->next) {
-    std::string name(lst->str);
+    name = std::string(lst->str);
+    
     bool breakflag = 0;
     for (int i = 0; i < elements.size(); i++)
       if (name == elements[i].name) {
@@ -35,14 +40,46 @@ ElementTable::ElementTable(Sand2Spec* spec) {
   }
 
   /*
+   * Step 2: Set up properties for each element
+   */
   for (S2List* node = spec->properties; node; node = node->next) {
-    Element element(node->str);
-    element.fixed = node->prop->fixed;
-    element.density = node->prop->density;
-    element.argb = node->prop->argb;
-    for (S2List* rnode = spec->prop->reactions; rnode; rnode = rnode->next) {
-      element.
+    name = std::string(node->str);
+
+    //Find element we're defining
+    for (i = 0; i < elements.size(); i++)
+      if (name == elements[i].name) break;
+    
+    elements[i].fixed = node->prop->fixed;
+    elements[i].density = node->prop->density;
+    elements[i].argb = node->prop->argb;
+    elements[i].reactions.resize(elements.size());
+
+    for (S2List* rnode = node->prop->reactions; rnode; rnode = rnode->next) {
+      //Element being reacted to
+      name = std::string(rnode->str);
+      for (j = 0; j < elements.size(); j++)
+	if (name == elements[j].name) break;
+      if (j == elements.size()) continue;
+ 
+      for (S2List* rnode_p = rnode->lst; rnode_p; rnode_p = rnode_p->next) {
+	//Result element
+	name = std::string(rnode_p->str);
+	for (k = 0; k < elements.size(); k++)
+	  if (name == elements[k].name) break;
+	if (k == elements.size()) continue;
+
+	elements[i].reactions[j].add(ProbEntry((ElementID)k, rnode_p->p));
+      }
+    }
+
+    for (S2List* dnode = node->prop->decay; dnode; dnode = dnode->next) {
+      //Element to decay to
+      name = std::string(dnode->str);
+      for (j = 0; j < elements.size(); j++)
+	if (name == elements[j].name) break;
+      if (j == elements.size()) continue;
+
+      elements[i].decay.add(ProbEntry((ElementID)j, dnode->p));
     }
   }
-  */ 
 }
